03d751b6b8a1c42bf6928f81eaa2377ec8eae67d
ciminoce <ciminoce@yahoo.com.ar>
2021-10-04 19:56:53 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio01Poo/Cuadrado.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01Poo
+{
+    class Cuadrado
+    {
+        //private int lado;
+
+        //public void SetLado(int value)
+        //{
+        //    if (value>0)
+        //    {
+        //        lado = value;
+        //    }
+        //    else
+        //    {
+        //        throw new Exception("El valor del lado debe ser positivo");
+        //    }
+        //}
+
+        //public int GetLado()
+        //{
+        //    return lado;
+        //}
+        //private int lado;
+
+        //public int Lado
+        //{
+        //    get { return lado; }
+        //    set
+        //    {
+        //        if (value > 0)
+        //        {
+        //            lado = value;
+        //        }
+        //        else
+        //        {
+        //            throw new Exception("El valor del lado debe ser positivo");
+        //        }
+        //    }
+        //}
+        public Cuadrado()
+        {
+
+        }
+
+        public Cuadrado(int valorDelLado)
+        {
+            Lado = valorDelLado;
+        }
+        public int Lado { get; set; }
+
+        public bool Validar()
+        {
+            return Lado > 0;
+        }
+
+        public double GetPerimetro()
+        {
+            return Lado * 4;
+        }
+
+        public double GetSuperficie()
+        {
+            return Math.Pow(Lado, 2);
+        }
+    }
+}
---
+++ Ejercicio01Poo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01Poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    Cuadrado c = new Cuadrado();
+            //    c.Lado = -5;
+
+            //    Console.WriteLine($"La medida del lado es {c.Lado}");
+
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    Console.WriteLine(ex.Message);
+            //}  
+            //Cuadrado c = new Cuadrado();
+            //c.Lado = 10;
+            Cuadrado c = new Cuadrado(10);
+            Cuadrado c2 = new Cuadrado();
+            Cuadrado c3 = new Cuadrado() {Lado = 15};
+            if (c.Validar())
+            {
+                Console.WriteLine($"La medida del lado es {c3.Lado}");
+                Console.WriteLine($"Su perímetro es {c3.GetPerimetro()}");
+                Console.WriteLine($"La superficie es {c3.GetSuperficie()}");
+                
+            }
+            else
+            {
+                Console.WriteLine("Objeto no válido");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
